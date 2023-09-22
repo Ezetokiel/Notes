0a8be41558028e5f31e0e4b4bd38fb4018ff43a3
Ezetokiel <kuah_yong_chuan_ezekiel@s2022.ssts.edu.sg>
2023-09-22 17:14:33 +0800

    first commit

---
+++ factor2.js
@@ -0,0 +1,151 @@
+const subFactorOne = `<p class="headers">Sub Factor 1: Affordable housing</p><br>
+<p>Affordable housing is housing that is priced at a reasonable cost, such that residents still have enough money for other basic needs</p><br>
+<p>Variety of housing types in Singapore cater to different income groups</p><br>
+<p>Singapore hopes to become an inclusive city by providing inclusive housing</p><br>
+<br>
+<p class="list-numbers">Different types of public housing meet the different needs of residents:</p>
+<p>1. Studio Apartments (catered to seniors)</p>
+<p>2. 3-room flats (catered to small families)</p><br>
+<br>
+<p class="list-numbers">*Financial schemes are available to subsidise cost of flats:*</p>
+<p>1. Additional CPF Housing Grant (AHG)</p>
+<p>2. Introduced in 2006 to help low-income families buy their first flat</p>
+<p>3. Families earning not more than S$5,000 a month can qualify for a grant of up to S$40,000 which is used to pay for the flat</p><br>
+<br>
+<p class="list-numbers">The Housing Development Board (HDB) plays a central role in providing affordable and quality housing for residents</p><br>
+<br>
+<p class="headers">Criteria: Housing Subsidy Options</p><br>
+<p class="list-numbers">1. HDB Subsidies:</p>
+<p>-Additional CPF Housing Grant (AHG) and Special CPF Housing Grant (SHG) are means-tested subsidies.</p>
+<p>-Aimed at lower- to middle-income households, they assist in purchasing an HDB flat.</p>
+<br>
+<p class="list-numbers">2. Rental Housing Assistance:</p>
+<p>-HDB provides subsidized rental housing options for those unable to afford home ownership.</p>
+<p>-Heavily subsidized rental flats ensure affordability for low-income families.</p>
+<br>
+<p class="list-numbers">3. CPF Housing Grants:</p>
+<p>-Central Provident Fund (CPF) Housing Grants offer financial assistance.</p>
+<p>-Grants like the Family Grant, Half-Housing Grant, and Proximity Housing Grant help eligible buyers offset HDB flat purchase costs.</p>
+<br>
+<p class="list-numbers">*4. Elderly Housing Options*:</p>
+<p>-Housing designed for elderly residents includes Studio Apartments (SAs) and 2-room Flexi flats. (Size)</p>
+<p>-These options cater to the unique needs of the elderly and often come at more affordable rates. (Money)</p>
+<br>
+<p class="list-numbers">5. Singles Housing Options (35 years and above)</p>
+<p>-For singles aged 35 and above, HDB offers various housing options, including Studio Apartments (SAs) and 2-room Flexi flat</p>
+<p>-These options are designed to cater to the housing needs of single individuals in this age group. (Based on single income)</p>
+<br>
+<button onclick="subFactorOneFunction()">Sub Factor 1</button>
+<button onclick="subFactorTwoFunction()">Sub Factor 2</button>
+<button onclick="subFactorThreeFunction()">Sub Factor 3</button>`
+
+const subFactorTwo = `<p class="headers">Sub Factor 2: A strong sense of belonging</p><br>
+<p>People may regard places with special meaning and remember these places fondly due to their experience or time there</p><br>
+<p>Pleasant experiences in a place make people more likely to develop a sense of place and belonging</p><br>
+
+<p class="headers">Strong sense of place and belonging</p><br>
+<p><i>“A sense of place refers to the meaning and value people attach to a place as a result of their experiences or the unique characteristics of a place.” </i></p><br>
+
+<p>Distinctive landmarks and physical features allow people to recognise the place easily</p><br>
+<p>It also helps evoke a sense of place and belonging for the residents</p><br>
+<br>
+<p class="list-numbers">*The Ethnic Integration Policy (EIP) is part of Singapore's public housing policy*:</p>
+<p>-The public housing promotes racial harmony and understanding by encouraging a mix of races in HDB estates.</p>
+<p>-Its main aim is to make sure that different racial groups live together in Housing and Development Board (HDB) estates.</p>
+<p>-The policy prevents one ethnic group from becoming the majority in any neighborhood.</p>
+<p>-It helps maintain a balanced mix of people from different races in each HDB estate.</p><br>
+
+<p class="list-numbers">EIP's Quota:</p>
+<p>- if Indian residents make up approximately 9% of the populatoin, roughly 9% of the flats in a given area will be designated for Indian households. (Chinese 74%, Malay 15%, others 25%)</p>
+<p>- based on this system there won't be any single ethnic group dominating a neighbourhood, which maintains a balanced mix of various races of people in each HDB estate</p><br>
+
+<p class="list-numbers">*Purpose of the EIP*:</p>
+<p>-The EIP is meant to promote racial harmony and integration among Singaporeans.</p>
+<p>-By ensuring a mix of races in HDB estates, it encourages people from different ethnic backgrounds to interact and get to know each other.</p>
+<p>-The ultimate goal is to build strong relationships among residents and create a harmonious society.</p>
+<button onclick="subFactorOneFunction()">Sub Factor 1</button>
+<button onclick="subFactorTwoFunction()">Sub Factor 2</button>
+<button onclick="subFactorThreeFunction()">Sub Factor 3</button>`
+
+const subFactorThree = `<p class="headers">Sub Factor 3: Facilities and Amenitites for all ages</p><br>
+<p>Inclusive housing means having facilities and amenities for all ages</p><br>
+<p class="list-numbers">Some common facilities and amenities found in neighbourhoods:</p>
+<p>- retail shops</p>
+<p>- clinics</p>
+<p>- parks</p><br>
+<p>3-Generation (3G) facilities cater to the needs of multi-generational families</p><br>
+
+<p class="list-numbers">The 3 Ages Groups:</p>
+<p>1. Children & Youth (under 16 years old) - Playgrounds</p>
+<p>2. Adults (17-64 years old) - Exercise corners</p>
+<p>3. Elderly (over 65 years old) - Community Centres</p><br>
+
+<p class="list-numbers">The 4 Special Groups:</p>
+<p>1. Pregnant Women (PW)</p>
+<p>2. Baby in Pram (BP)</p>
+<p>3. Visually impaired (VIP)</p>
+<p>4. Wheelchair-bound (WB)</p>
+
+<p>Facilities and amenities should also cater to seniors. For example: Corridors need to be at least 1.5m wide to facilitate access for people using mobility aids like wheelchairs</p><br>
+
+<p class="headers">*E.A.S.E 2.0 (Enhancement for Active Seniors) program*:</p><br>
+<p>*E.A.S.E 1.0 added ramps while E.A.S.E 2.0 added slip mats and grab bars</p>
+<p>Some of the features of the program includes:</p><br>
+
+<p class="list-numbers">1. Home Improvements:</p>
+<p>- offers various home improvement options for elderly households.</p> 
+<p>- This includes installing grab bars in bathrooms and non-slip flooring to prevent accidents and falls.</p>
+<p>- These changes make homes safer for seniors.</p><br>
+
+<p class="list-numbers">2. Non-Intrusive Modifications:</p>
+<p>- The programme focuses on non-intrusive modifications, which means that the changes made to the homes with minimal renovations.</p> 
+<p>- This makes it easier and more affordable to implement the improvements.</p><br>
+
+<p class="list-numbers">3. Financial Assistance:</p>
+<p>- E.A.S.E. 2.0 provides financial assistance to eligible elderly households to cover the costs of the home improvements.</p> 
+<p>- helps seniors who may have limited financial resources access the necessary changes to their homes.</p><br>
+
+<p class="list-numbers">4. Customized Solutions:</p>
+<p>- The programme offers customized solutions based on the specific needs of each household.</p>
+<p>- It takes into account the unique requirements of elderly residents, such as mobility and safety concerns.</p><br>
+
+<p class="list-numbers">5. Collaboration:</p>
+<p>- HDB collaborates with various agencies and partners to implement the programme effectively.</p>
+<p>- This ensures that seniors receive comprehensive support for their housing needs.</p><br>
+
+<p class="headers">Limitations of HDB's E.A.S.E. 2.0 Programme</p><br>
+<p class="list-numbers">1. Eligibility Criteria</p>
+<p>- To benefit from the programme, elderly households must meet specific eligibility criteria.</p>
+<p>- Some seniors who may need assistance may not qualify, which can be a limitation for those who do not meet the criteria.</p><br>
+
+<p class="list-numbers">2. Scope of Modifications</p>
+<p>- E.A.S.E. 2.0 focuses on non-intrusive modifications, which may not address all the needs of elderly residents.</p>
+<p>- Some seniors may require more extensive changes to their homes, which may not be covered by the programme.</p>
+<p>- Example: some seniors with severe mobility challenges might need a complete kitchen overhaul to accommodate a wheelchair or mobility aid</p><br>
+
+<button onclick="subFactorOneFunction()">Sub Factor 1</button>
+<button onclick="subFactorTwoFunction()">Sub Factor 2</button>
+<button onclick="subFactorThreeFunction()">Sub Factor 3</button>`;
+
+let subFactorDiv = document.querySelector(".sub-factors-container");
+
+function subFactorOneFunction(){
+    subFactorDiv.innerHTML = subFactorOne;
+    window.scrollTo({
+        top: 1150,
+    })
+}
+
+function subFactorTwoFunction(){
+    subFactorDiv.innerHTML = subFactorTwo; 
+    window.scrollTo({
+        top: 1150,
+    })
+}
+
+function subFactorThreeFunction(){
+    subFactorDiv.innerHTML = subFactorThree;
+    window.scrollTo({
+        top: 1150,
+    })
+}
